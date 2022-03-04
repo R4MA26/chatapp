691c8fcdc7359780a9460327fc6595f1ae04090a
R4MA26 <ramaismaya25@gmail.com>
2022-03-04 14:49:22 +0800

    ui profile

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -1,3 +1,4 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -8,15 +9,129 @@ class ProfileView extends GetView<ProfileController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('ProfileView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'ProfileView is working',
-          style: TextStyle(fontSize: 20),
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.logout,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            child: Column(
+              children: [
+                AvatarGlow(
+                  endRadius: 110,
+                  glowColor: Colors.black,
+                  duration: const Duration(seconds: 2),
+                  child: Container(
+                    margin: EdgeInsets.all(15),
+                    width: 175,
+                    height: 175,
+                    decoration: BoxDecoration(
+                      color: Colors.black38,
+                      borderRadius: BorderRadius.circular(100),
+                      image: DecorationImage(
+                          image: AssetImage("assets/logo/noimage.png"),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                ),
+                Text(
+                  "Rama",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  "rama@gmail.com",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            child: Container(
+              child: Column(
+                children: [
+                  ListTile(
+                    onTap: () {},
+                    leading: Icon(Icons.note_add_outlined),
+                    title: Text(
+                      "Update status",
+                      style: TextStyle(
+                        fontSize: 22,
+                      ),
+                    ),
+                    trailing: Icon(Icons.arrow_right),
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    leading: Icon(Icons.person),
+                    title: Text(
+                      "Change profile",
+                      style: TextStyle(
+                        fontSize: 22,
+                      ),
+                    ),
+                    trailing: Icon(Icons.arrow_right),
+                  ),
+                  ListTile(
+                    onTap: () {},
+                    leading: Icon(Icons.color_lens),
+                    title: Text(
+                      "Change theme",
+                      style: TextStyle(
+                        fontSize: 22,
+                      ),
+                    ),
+                    trailing: Text("Light"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin:
+                EdgeInsets.only(bottom: context.mediaQueryPadding.bottom + 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Chat App",
+                  style: TextStyle(color: Colors.black54),
+                ),
+                Text(
+                  "v.1.0",
+                  style: TextStyle(color: Colors.black54),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.done) {
           return GetMaterialApp(
             title: "chat app",
-            initialRoute: Routes.SEARCH,
+            initialRoute: Routes.PROFILE,
             getPages: AppPages.routes,
           );
           // return FutureBuilder(
